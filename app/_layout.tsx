ed166d2398c03f2185a5f027db299b9998dc722e
DebadritaGhosh <debadrita.ghosh2010@gmail.com>
2025-09-16 01:29:04 +0530

    Add: login and signup pages along with basic app navigation setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,16 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="auth/login" />
+        <Stack.Screen name="auth/signup" />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="auto" />
+    </>
+  );
 }
